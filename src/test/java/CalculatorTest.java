b6d82e3948766d1bcfb84c970250375afded4d5b
ilja <iljakristafenko@gmail.com>
2021-08-07 13:17:25 +0300

    [feature/TQA-6]: Implement Calculator

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -8,7 +8,7 @@ public class CalculatorTest {
 
     @Test
     public void addTest() {
-        assertEquals("Error, number is add not corect", 15.5, calculator.add(5.1, 10.4), 0);
+        assertEquals("Error, number is add not corect", 14.5, calculator.add(4.1, 10.4), 0);
     }
 
     @Test
